0e537dbec429339e189a5ec1c782ff3438db3c1c
Paxton Terry <117777648+PaxtonTerryDev@users.noreply.github.com>
2026-01-05 15:21:14 -0700

    Updates stdout config and default props

--- types/config/transports.ts
+++ types/config/transports.ts
@@ -1,33 +1,67 @@
-import { LogSegment } from "../../log.ts";
+import { LogLevel, LogSegment } from "../../log.ts";
 import { LogTransports } from "../../transports.ts";
 import type * as CSS from "csstype";
 
 export interface LogDashTransportsConfig {
-  [LogTransports.STDOUT]?: StdoutConfig;
+  [LogTransports.STDOUT]?: StdoutTransportConfig;
   [LogTransports.FILE]?: FileTransportConfig;
   [LogTransports.JSON]?: JSONTransportConfig;
 }
 
 interface TransportConfig {
-  enabled: true;
+  enabled: boolean;
   output?: Record<Partial<LogSegment>, boolean>;
 }
 
-interface StdoutConfig extends TransportConfig {
-  colors: {
-    enabled?: boolean;
-    segments?: Record<Partial<LogSegment>, CSS.Properties>;
-  };
+interface StdoutTransportConfig extends TransportConfig {
+  colors: Record<
+    Partial<LogLevel>,
+    Record<Partial<LogSegment>, Partial<CSS.Properties>>
+  >;
 }
 
-const defaultStdoutConfig: Required<StdoutConfig> = {
+const defaultStdoutTransportConfig: Required<StdoutTransportConfig> = {
   enabled: true,
   colors: {
-    enabled: true,
-    segments: {
+    [LogLevel.DEBUG]: {
       [LogSegment.TIMESTAMP]: {},
-      [LogSegment.LEVEL]: {},
-      [LogSegment.PREFIX]: {},
+      [LogSegment.LEVEL]: { backgroundColor: "gray", color: "white" },
+      [LogSegment.PREFIX]: { color: "gray" },
+      [LogSegment.MESSAGE]: {},
+      [LogSegment.METADATA]: {},
+    },
+    [LogLevel.TRACE]: {
+      [LogSegment.TIMESTAMP]: {},
+      [LogSegment.LEVEL]: { backgroundColor: "lightgray", color: "black" },
+      [LogSegment.PREFIX]: { color: "lightgray" },
+      [LogSegment.MESSAGE]: {},
+      [LogSegment.METADATA]: {},
+    },
+    [LogLevel.INFO]: {
+      [LogSegment.TIMESTAMP]: {},
+      [LogSegment.LEVEL]: { backgroundColor: "blue", color: "white" },
+      [LogSegment.PREFIX]: { color: "blue" },
+      [LogSegment.MESSAGE]: {},
+      [LogSegment.METADATA]: {},
+    },
+    [LogLevel.WARN]: {
+      [LogSegment.TIMESTAMP]: {},
+      [LogSegment.LEVEL]: { backgroundColor: "orange", color: "white" },
+      [LogSegment.PREFIX]: { color: "orange" },
+      [LogSegment.MESSAGE]: {},
+      [LogSegment.METADATA]: {},
+    },
+    [LogLevel.ERROR]: {
+      [LogSegment.TIMESTAMP]: {},
+      [LogSegment.LEVEL]: { backgroundColor: "red", color: "white" },
+      [LogSegment.PREFIX]: { color: "red" },
+      [LogSegment.MESSAGE]: {},
+      [LogSegment.METADATA]: {},
+    },
+    [LogLevel.FATAL]: {
+      [LogSegment.TIMESTAMP]: {},
+      [LogSegment.LEVEL]: { backgroundColor: "darkred", color: "white" },
+      [LogSegment.PREFIX]: { color: "magenta" },
       [LogSegment.MESSAGE]: {},
       [LogSegment.METADATA]: {},
     },
@@ -41,8 +75,88 @@ const defaultStdoutConfig: Required<StdoutConfig> = {
   },
 };
 
-interface FileTransportConfig extends TransportConfig {}
+type FileSplitMethod = "date" | "size";
 
-interface JSONTransportConfig extends TransportConfig {}
+// TODO: There is probably a pretty slick way to pass in a lambda that sets up the storage directories.
 
-export const defaultTransportConfig: Required<LogDashTransportsConfig> = {};
+interface FileTransportConfigBase extends TransportConfig {
+  outdir?: string;
+  splitMethod?: FileSplitMethod;
+}
+
+interface FileTransportConfigDateSplit extends FileTransportConfigBase {
+  splitMethod: "date";
+  /** Should be a valid cron string */
+  schedule: string;
+}
+
+type SizeSplitNamingType = "uuid" | "increment";
+
+interface FileTransportConfigSizeSplitBase extends FileTransportConfigBase {
+  splitMethod: "size";
+  /** Maximum size (Mb) of the file before splitting */
+  size?: number;
+}
+
+interface FileTransportConfigSizeSplitUUID
+  extends FileTransportConfigSizeSplitBase {
+  naming: {
+    type: "uuid";
+  };
+}
+
+interface FileTransportConfigSizeSplitIncrement
+  extends FileTransportConfigSizeSplitBase {
+  naming: {
+    type: "increment";
+    baseFileName?: string;
+  };
+}
+
+type FileTransportConfigSizeSplit =
+  | FileTransportConfigSizeSplitUUID
+  | FileTransportConfigSizeSplitIncrement;
+
+type FileTransportConfig =
+  | FileTransportConfigDateSplit
+  | FileTransportConfigSizeSplit;
+
+const defaultFileTransportConfig: Required<FileTransportConfig> = {
+  enabled: false,
+  output: {
+    [LogSegment.TIMESTAMP]: true,
+    [LogSegment.LEVEL]: true,
+    [LogSegment.PREFIX]: true,
+    [LogSegment.MESSAGE]: true,
+    [LogSegment.METADATA]: false,
+  },
+  outdir: "logs/",
+  splitMethod: "date",
+  schedule: "* * 1 * *",
+};
+
+interface JSONTransportConfig extends TransportConfig {
+  endpoint: string;
+  headers: [string, string][];
+  body: [string, string][];
+}
+
+const defaultJSONTransportConfig: Required<JSONTransportConfig> = {
+  enabled: false,
+  output: {
+    [LogSegment.TIMESTAMP]: true,
+    [LogSegment.LEVEL]: true,
+    [LogSegment.PREFIX]: true,
+    [LogSegment.MESSAGE]: true,
+    [LogSegment.METADATA]: true,
+  },
+  endpoint: "",
+  headers: [],
+  body: [],
+};
+
+export const defaultTransportConfig: Required<LogDashTransportsConfig> = {
+  [LogTransports.STDOUT]: defaultStdoutTransportConfig,
+  [LogTransports.FILE]: defaultFileTransportConfig,
+  [LogTransports.JSON]: defaultJSONTransportConfig,
+};
