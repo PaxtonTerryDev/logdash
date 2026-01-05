5a8ef33b2269b03bbb1806dc829d687895457cc4
Paxton Terry <117777648+PaxtonTerryDev@users.noreply.github.com>
2026-01-05 15:35:51 -0700

    Adds exports from main file

--- main.ts
+++ main.ts
@@ -1,1 +1,5 @@
-
+export { loadConfig } from "./config-loader.ts";
+export type { LogDashConfig, LogDashConfigRootDef } from "./types/config/logdash.ts";
+export type { LogDashTransportsConfig } from "./types/config/transports.ts";
+export { LogLevel, LogSegment } from "./log.ts";
+export { LogTransports } from "./transports.ts";
