52e97944aa35231ebb960546285b76219d34c6e8
Paxton Terry <117777648+PaxtonTerryDev@users.noreply.github.com>
2026-01-05 11:59:13 -0700

    Extends Stdout config def and default

--- types/config/transports.ts
+++ types/config/transports.ts
@@ -2,23 +2,47 @@ import { LogSegment } from "../../log.ts";
 import { LogTransports } from "../../transports.ts";
 import type * as CSS from "csstype";
 
-interface LogDashTransportsConfig {
-  [LogTransports.STDOUT]: StdoutConfig;
-  [LogTransports.FILE]: FileTransportConfig;
-  [LogTransports.JSON]: JSONTransportConfig;
+export interface LogDashTransportsConfig {
+  [LogTransports.STDOUT]?: StdoutConfig;
+  [LogTransports.FILE]?: FileTransportConfig;
+  [LogTransports.JSON]?: JSONTransportConfig;
 }
 
 interface TransportConfig {
   enabled: true;
+  output?: Record<Partial<LogSegment>, boolean>;
 }
 
 interface StdoutConfig extends TransportConfig {
   colors: {
     enabled?: boolean;
-    segments?: Record<keyof LogSegment, CSS.Properties>;
+    segments?: Record<Partial<LogSegment>, CSS.Properties>;
   };
 }
+
+const defaultStdoutConfig: Required<StdoutConfig> = {
+  enabled: true,
+  colors: {
+    enabled: true,
+    segments: {
+      [LogSegment.TIMESTAMP]: {},
+      [LogSegment.LEVEL]: {},
+      [LogSegment.PREFIX]: {},
+      [LogSegment.MESSAGE]: {},
+      [LogSegment.METADATA]: {},
+    },
+  },
+  output: {
+    [LogSegment.TIMESTAMP]: false,
+    [LogSegment.LEVEL]: true,
+    [LogSegment.PREFIX]: true,
+    [LogSegment.MESSAGE]: true,
+    [LogSegment.METADATA]: false,
+  },
+};
 
 interface FileTransportConfig extends TransportConfig {}
 
 interface JSONTransportConfig extends TransportConfig {}
+
+export const defaultTransportConfig: Required<LogDashTransportsConfig> = {};
